67513573c4ec4e0da342d6ad70f738bc142eaeb8
Jinho(David) Seo <brownkpt60@gmail.com>
2022-12-15 16:49:55 -0500

    updates

--- tutorial/extract compoment/app.js
+++ tutorial/extract compoment/app.js
@@ -1,13 +1,41 @@
 // Extract Components
 
+function ProductInfo(props){
+    return(
+        <ol className ="productInfo">
+            <list className="product-model">{props.model}</list>
+            <list className="product-maker">{props.maker}</list>
+            <list className="product-year">{props.year}</list>
+            <list className="product-wheel">{props.wheel}</list>
+            <list className="product-engine">
+                < EngineSpec 
+                    model={props.EngineSpec}
+                />
+            </list>
+        </ol>
+    );
+}
+
+function EngineSpec(props){
+    return(
+        <ul className="engine-spec">
+            <list className="engine-model">{props.model.model}</list> 
+            <list className="engine-cylinder">{props.model.cylinder}</list>
+            <list className="engine-horsepower">{props.model.horsepower}</list>
+            <list className="engine-mode">{props.model.mode}</list>
+        </ul>
+    );
+}
 
 // product object
 const product = {
+    model: "HR-V",
     maker: "honda",
     year: "2022",
     wheel: "4",
     engine:
     {
+        model: "TurboX2",
         cylinder: 4,
         horsepower: 150,
         mode: "FWD",
@@ -15,11 +43,12 @@ const product = {
 }
 
 // root render element
-const root = ReactDom.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    < Product 
+    < product
+        model = {product.model} 
         maker = {product.maker}
-        year = {proudct.year}
+        year = {product.year}
         wheel = {product.wheel}
         engine = {product.engine}
     />
