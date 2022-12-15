86985614319d80aedbbd1032713676460570b380
Jinho(David) Seo <brownkpt60@gmail.com>
2022-12-15 12:45:03 -0500

    add new file/updates

--- tutorial/Rendering an Element into the DOM/app.js
+++ tutorial/Rendering an Element into the DOM/app.js
@@ -1,23 +1,120 @@
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+function Square(props){
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-const element = <h1>Hello, world</h1>;
-root.render(element);
+class Board extends React.Component {
+  
+  renderSquare(i) {
+    return <Square value = {this.props.squares[i]}
+             onClick={()=> this.props.onClick(i)}/>;
+  }
 
-// -----------------------------------
-
-class BaseStructure extends React.Component {
-  render() {
+  render(){
     return (
-      <div className="Base">
-        <h1>Base Structure</h1>
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
 }
 
-const root1 = ReactDOM.createRoot(
-    document.getElementById('root1')
-);
-root1.render(<BaseStructure/>)
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i){
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    const squares = this.state.squares.slice();
+    
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    
+    squares[i] = this.state.xIsNext? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
+  render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner){
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next Player: ' + (this.status.xIsNext ? 'X' : 'O');
+    }
+    
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            sqaures = {current.squares}
+            onClick = {(i)=> this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{ status }</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
---
+++ tutorial/component/app.js
@@ -0,0 +1,28 @@
+// Component:
+// Small and isolated block of code
+
+
+
+// Properties(Props) :
+// 
+// 
+// 
+
+
+// React Component Class (or React Compoment Type), taking parameter called Porps
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+// Example usage: <ShoppingList name="Mark" />
