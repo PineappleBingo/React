7448a55d90fd41e147d7895bdc46c57732a25df5
Jinho(David) Seo <brownkpt60@gmail.com>
2022-12-15 13:03:31 -0500

    updates

--- tutorial/component/app.js
+++ tutorial/component/app.js
@@ -2,6 +2,36 @@
 // Small and isolated block of code
 
 
+// Simplest form of component | called "Function Components" == JavaScript Function
+// which accepts single Props with data and returns React element
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+// Equivalent in ES6 class
+class Welcome extends React.Component {
+    
+    render() {
+    
+        const title = ' Staff Analyst I';
+        return <h1>Hello, {this.props.name} {title}</h1>;
+    // this refers to Window Object(Global Scope)(?)
+  }
+}
+
+// Render example code above
+// https://codepen.io/gaearon/pen/YGYmEG?editors=1010
+
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+const element = <Welcom name="Sara" />;
+
+root.render(element)
+
+
+
+
+
 
 // Properties(Props) :
 // 
