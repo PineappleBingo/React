bdab537b45cade62889a9a488963b2226fc0a689
Jinho(David) Seo <brownkpt60@gmail.com>
2022-12-16 17:12:38 -0500

    updates

--- tutorial/class-component-app/src/App.js
+++ tutorial/class-component-app/src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Class Component
+// Class components are more complex than functional components. 
+// It requires you to extend from React. Component and create a render function which 
+// returns a React element. You can pass data from one class to other class components. 
+// You can create a class by defining a class that extends Component and has a render function. 
+// Valid class component is shown in the below example.
+
+
+// syntax
+// class MyComponent extends React.Component {  
+//   render() {  
+//     return (  
+//       <div>This is main component.</div>  
+//     );  
+//   }  
+// }  
+
+
+// ES6 Syntax
+function App extends React.Component {
+  constructor{
+    super();
+    this.state ={
+      data:
+      [
+        {
+          "name": "David1",
+        },
+        {
+          "name": "David2",
+        },
+        {
+          "name": "David3",
+        }
+        ,
+      ]
+    }
+  }
+  render(
+    <div>
+      <StudentName />
+      <ul>
+        {this.state.data.map((item) => <List data = {item}/>)}
+      </ul>
     </div>
   );
 }
+
+class StudentName extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Student Name Detail</h1>
+      </div>
+
+    );
+  }
+}
+
 
 export default App;
