17becdd8bb0b101d8ec44054c17e7272d91a1dba
Jinho(David) Seo <brownkpt60@gmail.com>
2022-12-29 12:32:00 -0500

    updates

--- tutorial/date-app/src/App.js
+++ tutorial/date-app/src/App.js
@@ -2,22 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const current = new Date();
+  const date = current.getDate()+ '/' + current.getMonth() + '/' + current.getFullYear();
+  // console.log(typeof(date))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div>
+      <span>opt1. Date().toLocalDateString()</span>
+      <h1>{new Date().toLocaleDateString()+""}</h1>
+      <h1>{new Date().toLocaleDateString("en-us", {month: "short"})}</h1>
+      <h1>{new Date().toLocaleDateString("en-us", {day: "2-digit"})}</h1>
+      <h1>{new Date().getFullYear()}</h1>
+
+      <hr/>
+      <span>Opt2. getDate()/getMonth()/getFullYear()</span>
+      <h2>{date}</h2>
+
+      <hr/>
+      <span>International Date&Time : Intl.DateTimeFormat()</span>
+      <h3>{new Intl.DateTimeFormat().format(current)}</h3>
+      <h3>{new Intl.DateTimeFormat('es-US').format(current)}</h3>
+      <h3>{new Intl.DateTimeFormat('ko-KR', { dateStyle: 'full', timeStyle: 'short', timeZone: 'Asia/Seoul'}).format(current)}</h3>
     </div>
   );
 }
