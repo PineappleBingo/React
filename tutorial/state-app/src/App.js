36fc514111567df676b79b92b9b1f6f124460a2d
Jinho(David) Seo <brownkpt60@gmail.com>
2023-01-05 09:31:17 -0500

    variety of binding handle()

--- tutorial/state-app/src/App.js
+++ tutorial/state-app/src/App.js
@@ -20,17 +20,44 @@ import React, {Component} from 'react'
 // To set the state, it is required to call the super() method in the constructor. 
 // It is because this.state is uninitialized before the super() method has been called.
 
+// If you forget to bind this.handleClick and pass it to onClick, this will be undefined when the function is actually called.
+
+// This is not React-specific behavior; it is a part of how functions work in JavaScript. Generally, 
+// if you refer to a method without () after it, such as onClick={this.handleClick}, you should bind that method.
+
+// https://reactjs.org/docs/faq-functions.html
+
+
 class App extends React.Component {
   
-  constructor(){
-    super();
-    this.state = { isDataIn: false }
+  constructor(props){
+    super(props);
+    this.state = { 
+      isDataIn: false,
+      isTrue: true,
+      isYes: false,
+      isYYES: true,
+    }
     console.log("Component This:", this);
+
+    // This binding is necessary to make "this" work in the callback.
     this.toggleData = this.toggleData.bind(this);
   }
 
   toggleData(){
     this.setState({isDataIn: !this.state.isDataIn});
+  }
+  // Work-around opt.1 - Bind in Constructor
+  toggleData2 = () => {
+    this.setState({isTrue: !this.state.isTrue});
+  }
+  // Work-around opt.2 - Public Class Syntax
+  toggleData3(){
+    this.setState({isYes: !this.state.isYes});
+  }
+  // Work-around opt.3 - Bind in Render
+  toggleData4(){
+    this.setState({isYYES: !this.state.isYYES});
   }
 
   render(){ 
@@ -42,9 +69,23 @@ class App extends React.Component {
       ); 
   
       return (
-        <div ClassName="MessageBox">
-          <h1>Checking Data: {Mssg}</h1>
+        <div className="MessageBox">
+          <h1>Checking Data1 - Bind in Constructor(ES2015) : {Mssg}</h1>
           <button onClick={this.toggleData}> Click </button>
+          <hr />
+          <h1>Checking Data2 - Class Properties(ES2022) : {this.state.isTrue? 'On' : 'Off'}</h1>
+          <button onClick={this.toggleData2}> Click</button>
+          <hr />
+          <h1>Checking Data3 - Using Arrow Function Callback : {this.state.isYes? 'Yes' : 'No'}</h1>
+          {/* <button onClick={()=> this.toggleData3()}> Click</button> */}
+          {/* 
+          Using an arrow function in render creates a new function each time the component renders,
+          which may break optimizations based on strict identity comparison. 
+          */}
+          <hr />
+          <h1>Checking Data4 - Bind in Render : {this.state.isYYES? 'Yes' : 'No'}</h1>
+          <button onClick={this.toggleData4.bind(this)}> Click</button>          
+
         </div>
       );
   }
