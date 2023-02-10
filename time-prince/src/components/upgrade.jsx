692140794789b445f920b4ff94f646a496522f3b
Jinho(David) Seo <brownkpt60@gmail.com>
2023-02-10 15:35:51 -0500

    Add files via upload

---
+++ time-prince/src/components/upgrade.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class Upgrade extends Component {
+  // Think of it as private local variable
+  state = {
+    // name: this.props.name,
+    // energy: this.props.energy,
+    // description: this.props.description,
+    // imageUrl: this.props.imageUrl,
+    count: this.props.count,
+  };
+
+  //   //   one solution
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    // console.log("Clicked");
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    // console.log(this.props);
+    return (
+      <div>
+        {/* directly getting name from props */}
+        <h2>{this.props.name}</h2>
+        <div>{this.props.energy}</div>
+        <img src={this.props.imageUrl} alt="" />
+        <div>{this.props.description}</div>
+        <span className="badge badge-warning m-2">{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          +
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    // return this.state.count === 0 ? "Zero" : this.state.count;
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Upgrade;
---
+++ time-prince/src/components/upgrades.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import Upgrade from "./upgrade";
+
+// const upgrades = {
+//   1: {
+//     id: 1,
+//     name: "Rose Tree",
+//     count: 0,
+//     energy: 600,
+//     description: "This is rose tree..",
+//     imageUrl: "https://picsum.photos/200",
+//   },
+//   2: {
+//     id: 2,
+//     name: "Olive Tree",
+//     count: 0,
+//     energy: 400,
+//     description: "This is Olive tree..",
+//     imageUrl: "https://picsum.photos/200",
+//   },
+//   3: {
+//     id: 3,
+//     name: "Tree House",
+//     count: 0,
+//     energy: 300,
+//     description: "This is Tree House..",
+//     imageUrl: "https://picsum.photos/200",
+//   },
+// };
+
+class Upgrades extends Component {
+  state = {
+    // upgrades: upgrades,
+
+    upgrades: [
+      {
+        id: 1,
+        name: "Rose Tree",
+        count: 0,
+        energy: 600,
+        description: "This is rose tree..",
+        imageUrl: "https://picsum.photos/200",
+      },
+      {
+        id: 2,
+        name: "Olive Tree",
+        count: 0,
+        energy: 400,
+        description: "This is Olive tree..",
+        imageUrl: "https://picsum.photos/200",
+      },
+      {
+        id: 3,
+        name: "Tree House",
+        count: 0,
+        energy: 300,
+        description: "This is Tree House..",
+        imageUrl: "https://picsum.photos/200",
+      },
+    ],
+  };
+
+  //   rules of thumbs - modify state within in the component declared.
+  // instead of we delete upgrade object in upgrade component, we "raising" & "Handling" Event
+  handleDelete = (upgradeId) => {
+    // do not update states directly.
+    console.log("Event Handler Called:", upgradeId);
+    const upgrades = this.state.upgrades.filter((u) => u.id !== upgradeId);
+    // this.setState({ upgrades : upgrades})
+    this.setState({ upgrades });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.upgrades.map((upgrade) => (
+          <Upgrade
+            key={upgrade.id}
+            id={upgrade.id}
+            name={upgrade.name}
+            energy={upgrade.energy}
+            description={upgrade.description}
+            imageUrl={upgrade.imageUrl}
+            count={upgrade.count}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Upgrades;
